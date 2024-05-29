30deef2c8f331a31db108658434a568fee2a6596
juanpintodev <166082294+juanpintodev@users.noreply.github.com>
2024-05-28 20:58:54 -0400

    lista contactos final BDjsonserver y preloader

---
+++ home/index.js
@@ -0,0 +1,61 @@
+const formCreate = document.querySelector('#form-create');
+const createInput = document.querySelector('#create-input');
+const formLogin = document.querySelector('#form-login');
+const loginInput = document.querySelector('#login-input');
+const notification = document.querySelector('.notification');
+const container = document.querySelector('#container');
+formLogin.addEventListener('submit', async e =>{
+    e.preventDefault();
+
+    const response = await fetch('http://localhost:3000/users', {method: 'GET'});
+    const users = await response.json();
+    const user = users.find(user => user.username === loginInput.value);
+
+    if (!user) {
+        notification.innerHTML =`El usuario no existe`;
+        notification.classList.add('show-notification');
+        setTimeout(() =>{
+            notification.classList.remove('show-notification');
+        }, 1500);
+    } else {
+        localStorage.setItem('user', JSON.stringify(user));
+        container.innerHTML = `<div class="lds-ring"><div></div><div></div><div></div><div></div></div>`;
+        window.location.href = '../listacontactos/lista.html';
+    }
+})
+
+formCreate.addEventListener('submit', async e =>{
+    e.preventDefault();
+
+    const response = await fetch('http://localhost:3000/users', {method: 'GET'});
+    const users = await response.json();
+    const user = users.find(user => user.username === createInput.value);
+
+    if(!createInput.value){
+        notification.innerHTML =`El campo usuario no puede estar vacio`;
+        notification.classList.add('show-notification');
+        setTimeout(() =>{
+            notification.classList.remove('show-notification');
+        }, 3000);
+    }else if(user){
+        notification.innerHTML =`El nombre de usuario ingresado ya existe`;
+        notification.classList.add('show-notification');
+        setTimeout(() =>{
+            notification.classList.remove('show-notification');
+        }, 3000);
+    }else{
+        await fetch('http://localhost:3000/users', {
+            method: 'POST',
+            headers: {'Content-Type':'aplication/json'},
+            body: JSON.stringify({username: createInput.value}),
+        })
+        notification.innerHTML =`El usuario ${createInput.value} ha sido creado con exito`;
+        notification.classList.add('show-notification');
+        container.innerHTML = `<div class="lds-ring"><div></div><div></div><div></div><div></div></div>`;
+        setTimeout(() =>{
+            notification.classList.remove('show-notification');
+            window.location.href = 'index.html';
+        }, 1000);
+        createInput.value = ``;
+    }
+});
